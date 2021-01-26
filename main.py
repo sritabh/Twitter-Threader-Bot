d3e5844ca4bddad9ff58d76dad86154c645507d0
SobyDamn <sobydanny@gmail.com>
2021-01-26 21:03:07 +0530

    Added Class Methods ThreadCompiler FirebaseUtils

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import tweepy
 import time
 import logging
 import random
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 import os
 from os import environ
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +18,113 @@ auth = tweepy.OAuthHandler(API_KEY,API_SKEY)
 auth.set_access_token(ACC_Token,ACC_Token_Secret)
 api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
+cred = credentials.Certificate("twitter-threader-firebase-adminsdk-rh7d4-60df64a83c.json") #For Firebase
+
+class userThread:
+    def __init__(self,id, name,username,profile_img,tweets):
+        self.id = id
+        self.name = name
+        self.username = username
+        self.tweets = tweets
+        self.profile_img = profile_img
+    def to_dict(self):
+        obj = {
+            'id':self.id,
+            'name':self.name,
+            'username':self.username,
+            'profile_img':self.profile_img,
+            'tweets':{}
+        }
+        for tweet in self.tweets:
+            tweet_id = tweet.tweet_id
+            obj['tweets'][str(tweet_id)] = tweet.to_dict()
+        return obj
+class Tweet:
+    def __init__(self, text,date,medias,tweet_id,urls):
+        self.text = text
+        self.date = date
+        self.medias = medias
+        self.tweet_id = tweet_id
+        self.urls = urls ##contains list either empty or list with object url,expanded_url,display_url(sliced urls)
+    def to_dict(self):
+        obj = {
+            'text':self.text,
+            'date':self.date,
+            'tweet_id':self.tweet_id,
+            'urls':self.urls,
+            'medias':self.medias
+        }
+        return obj
+class ThreadCompiler:
+    def __init__(self,tweet_id,user_id):
+        self.tweet_id = tweet_id
+        self.id = tweet_id ##storing parent id
+        self.user_id = user_id
+    def compile(self):
+        tweets = []
+        parent_tweet_id = self.tweet_id
+        parent_id = self.user_id
+        print("ThreadCompiler: Fetching Tweets")
+        while parent_tweet_id:
+            tweet = api.get_status(parent_tweet_id, tweet_mode="extended")
+            if tweet.user.id != parent_id:
+                break
+            medias = []
+            if 'media' in tweet.entities:
+                media_entities = tweet.entities['media']
+                for media_data in media_entities:
+                    ##Currently only planning to save photo data as it'll be transformed to PDF
+                    if media_data['type']=='photo':
+                        medias.append(media_data['media_url_https'])
+            tweetObj = Tweet(tweet.full_text,str(tweet.created_at),list(medias),parent_tweet_id,tweet.entities['urls'])
+            tweets.append(tweetObj)
+            parent_tweet_id = tweet.in_reply_to_status_id
+        return self.compileUser(list(tweets),parent_id)
+    def compileUser(self,tweets,user_id):
+        print("ThreadCompiler: Compiling Tweets and User")
+        tweets = tweets[::-1]
+        self.id = tweets[0] ##first id is used to save the thread
+        user = api.get_user(user_id)
+        return userThread(user_id,user.name,user.screen_name,user.profile_image_url_https,tweets)
+    def save(self,threaData=None):
+        if not threaData:
+            threaData = self.compile().to_dict()
+        print("ThreadCompiler: Preparing to save!")
+        FU = FirebaseUtility(cred)
+        FU.storeData(self.id,threaData)
+        print("ThreadCompiler: Preparing to save!")
+        return True
+    def getThreadID(self):
+        return str(self.id)
+
+class FirebaseUtility:
+    def __init__(self,cred):
+        self.cred = cred
+        self.db = firestore.client()
+        self.initialize()
+    def initialize(self):
+        try:
+            if not firebase_admin._apps:
+                print("FirebaseUtility:Initializing")
+                firebase_admin.initialize_app(self.cred)
+        except:
+            print("FirebaseUtility:Error in cred FIX NEEDED!")
+    def documentExists(self,thread_id):
+        doc_ref = self.db.collection(u'threads').document(str(thread_id))
+        doc = doc = doc_ref.get()
+        if doc.exists:
+            print("FirebaseUtility:Thread Already Exists")
+            return True
+        else:
+            print('FirebaseUtility:No such Thread!')
+            return False
+    def storeData(self,thread_id,data):
+        if not self.documentExists(thread_id):
+            doc_ref = self.db.collection(u'threads').document(str(thread_id))
+            doc_ref.set(data)
+            print('FirebaseUtility:Thread {} Stored!'.format(str(thread_id)))
+        else:
+            pass
 def retrieve_since_id(file_name="since_id.txt"):
     fread = open(file_name, 'r')
     since_id = int(fread.read().strip())
@@ -26,30 +136,14 @@ def store_since_id(since_id, file_name="since_id.txt"):
     fwrite.write(str(since_id))
     fwrite.close()
     return
-since_id = retrieve_since_id()
 def fetchTweets():
     ##Fetching only mentioned tweet
     #Retweet will also trigger this
     since_id = retrieve_since_id()
-    mentions = api.mentions_timeline(since_id,tweet_mode='extended')
-    mention = mentions[-1]
+    mentions = api.mentions_timeline(since_id)
+    mention = mentions[-1] if len(mentions) !=0 else None
     if mention:
         since_id = mention.id #Store the last id so that we can keep ourself updated
         store_since_id(since_id)
     return mention
-tweet = fetchTweets()
-replied_to_user_name = tweet.in_reply_to_screen_name
-replied_to_user_id = tweet.in_reply_to_user_id
-replied_to_id = tweet.in_reply_to_status_id
-recent_tweets = tweepy.Cursor(api.user_timeline,replied_to_user_id,since_id=replied_to_id).pages(1)
-def saveData(data, file_name="data.txt"):
-    fwrite = open(file_name, 'a',encoding="utf-8")
-    fwrite.write("\n")
-    fwrite.write(str(data))
-    fwrite.close()
-    return
-for page in recent_tweets:
-    for item in page:
-        saveData(item)
-        break
         
